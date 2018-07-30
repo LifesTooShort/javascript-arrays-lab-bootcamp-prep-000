11e55068a589f74e5e8d592273fcec2f7a5cdd2d
Erin Penner <epenner0522@gmail.com>
2018-07-30 17:22:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  return slice(name);
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten(name) {
